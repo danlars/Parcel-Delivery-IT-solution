fa5c7d735fcb0a83b16cf708ccb9ea78c8d14447
Andreas Abildtrup Hansen <aah@netcompany.com>
2021-06-17 13:56:11 +0200

    Added initial controllers

---
+++ Parcel Delivery IT solution/Controllers/CustomerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Parcel_Delivery_IT_solution.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class BusinessLogicController : ControllerBase
+    {
+        public BusinessLogicController(){}
+
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
