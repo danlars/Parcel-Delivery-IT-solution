ce78d689fe47b798d7ce4b87fb7b623c7fae25d0
Andreas Abildtrup Hansen <aah@netcompany.com>
2021-06-17 20:15:47 +0200

    Databaseclient

---
+++ Parcel Delivery IT solution/Controllers/EisApiExternal.cs
@@ -0,0 +1,33 @@
+using ProductsApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web.Http;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Parcel_Delivery_IT_solution.Controllers
+{
+    public class ValuesController : ApiController
+    {
+        static string _address = "http://api.worldbank.org/countries?format=json";
+        private string result;
+
+        // GET api/values
+        public IEnumerable<string> Get()
+        {
+            GetResponse();
+            return new string[] { result, "value2" };
+        }
+
+        private async void GetResponse()
+        {
+            var client = new HttpClient();
+            HttpResponseMessage response = await client.GetAsync(_address);
+            response.EnsureSuccessStatusCode();
+            result = await response.Content.ReadAsStringAsync();
+        }
+    }
+}
--- Parcel Delivery IT solution/DataAccess/DbClient.cs
+++ Parcel Delivery IT solution/DataAccess/DbClient.cs
@@ -6,9 +6,20 @@ using System.Threading.Tasks;
 using System.Data.SqlClient;
 
 namespace Parcel_Delivery_IT_solution
-class DbClient
 {
-    public Customer getCustomer(int ID)
+    class DbClient
     {
+        public Customer getCustomer(int ID)
+        {
+            Customer customer = new Customer();
+            customer.CityOfResidence = "Copenhagen";
+            customer.ZipCode = "1234565";
+            customer.Email = "some@mail.com";
+            customer.Name = "Anders And";
+            customer.TelephoneNumber = "123456";
+            customer.RegistrationNumber = 1234567;
+
+            return customer
+        }
     }
 }
