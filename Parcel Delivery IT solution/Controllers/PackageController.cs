7a825dcbc0d9a258f9d57cd1d72a5883d627ac13
Andreas Abildtrup Hansen <aah@netcompany.com>
2021-06-17 15:05:33 +0200

    Parcel delivery controller

---
+++ Parcel Delivery IT solution/Controllers/PackageController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace Parcel_Delivery_IT_solution.Controllers
+{
+    [ApiController]
+    [Microsoft.AspNetCore.Mvc.Route("[controller]")]
+    public class PackageController : Microsoft.AspNetCore.Mvc.ControllerBase
+    {
+        public PackageController(){}
+
+        [Microsoft.AspNetCore.Mvc.HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public Boolean InsertPackage(RouteDTO routeDTO)
+        {
+            
+            // Insert RouteDTO to database
+
+            return true;
+        }
+    }
+}
