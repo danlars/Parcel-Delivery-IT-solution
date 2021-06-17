8b539431a52541ae203f6f54eda295dae0888dc8
Andreas Abildtrup Hansen <aah@netcompany.com>
2021-06-18 01:26:41 +0200

    client

--- Parcel Delivery IT solution/Access/DbClient.cs
+++ Parcel Delivery IT solution/Access/DbClient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
 using System.Data.SqlClient;
 
 namespace Parcel_Delivery_IT_solution
@@ -12,15 +13,47 @@ namespace Parcel_Delivery_IT_solution
         public Customer getCustomer(int ID)
         {
             Customer customer = new Customer();
-            customer.CityOfResidence = "Copenhagen";
-            customer.ZipCode = "1234565";
-            customer.Email = "some@mail.com";
-            customer.Name = "Anders And";
-            customer.TelephoneNumber = "123456";
-            customer.RegistrationNumber = 1234567;
-            customer.CustomerId = 1;
+            customer.CustomerId = ID;
+            using (SqlConnection connection = new SqlConnection("Server=db-eit-dk1.database.windows.net\\MSSQLSERVER;Database=db-eit-dk1;User Id=admin-eit-dk1;Password=Eastindia4thewin;"))
+            {
+                string SqlCmdCustRegNo = "SELECT [RegistrationNumber], [Email], [CustommerName], [PhoneNumber], [ZipCode], [CityOfResidence] FROM Customer WHERE Customer_ID =" + ID.ToString();
+                SqlCommand command = new SqlCommand(SqlCmdCustRegNo, connection);
+                connection.Open();
 
+                SqlDataReader reader = command.ExecuteReader();
+
+                // Call Read before accessing data.
+                while (reader.Read())
+                {
+                    string regno = reader[0].ToString();
+                    string email = reader[1].ToString();
+                    string CustName = reader[2].ToString();
+                    string PhoneNumber = reader[3].ToString();
+                    string ZipCode = reader[4].ToString();
+                    string CityOfResidence = reader[5].ToString();
+
+                    customer.RegistrationNumber = Int32.Parse(regno);
+                    customer.Email = email;
+                    customer.Name = CustName;
+                    customer.TelephoneNumber = PhoneNumber;
+                    customer.ZipCode = ZipCode;
+                    customer.CityOfResidence = CityOfResidence;
+                }
+                // Call Close when done reading.
+                reader.Close();
+            }
             return customer;
+        }
+
+        public static void SetOrder(Order order)
+        {
+            string cmdString = "INSERT INTO [OrderTable] ([[Price], [PackageY], [PackageX], [PackageZ], [ShipmentDate], [FromCity], [ToCity]) VALUES (" + Convert.ToString(order.Price) + "," + Convert.ToString(order.Height) + "," + Convert.ToString(order.Width) + "," + Convert.ToString(order.Length) + "," + order.Date + "," + order.CityFrom + "," + order.CityTo + ")";
+            using (SqlConnection connection = new SqlConnection("Server=db-eit-dk1.database.windows.net\\MSSQLSERVER;Database=db-eit-dk1;User Id=admin-eit-dk1;Password=Eastindia4thewin;"))
+            {
+                SqlCommand command = new SqlCommand(cmdString, connection);
+                command.Connection.Open();
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
--- Parcel Delivery IT solution/Models/Order.cs
+++ Parcel Delivery IT solution/Models/Order.cs
@@ -1,4 +1,8 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Parcel_Delivery_IT_solution
 {
@@ -17,5 +21,17 @@ namespace Parcel_Delivery_IT_solution
         public int Price { get; set; }
 
         public int Time { get; set; }
+
+        public int Height { get; set; }
+
+        public int Length { get; set; }
+
+        public int Width { get; set; }
+
+        public int Date { get; set; }
+
+        public string CityFrom { get; set; }
+
+        public string CityTo { get; set; }
     }
 }
