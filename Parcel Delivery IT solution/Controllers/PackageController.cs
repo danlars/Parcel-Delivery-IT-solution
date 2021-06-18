fa4c9157d474c14007f82384604bc61e780d2881
Andreas Abildtrup Hansen <aah@netcompany.com>
2021-06-18 10:58:15 +0200

    changed controller

--- Parcel Delivery IT solution/Controllers/PackageController.cs
+++ Parcel Delivery IT solution/Controllers/PackageController.cs
@@ -14,22 +14,8 @@ namespace Parcel_Delivery_IT_solution.Controllers
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         public Boolean InsertPackage(int customerId, string email, string name, string telephoneNumber, int price, int time, int height, int length, int width, string cityFrom, string cityTo, string date)
-        {// int, string, string, string, int, int, int, int, int, string, string, string
+        {
             Order order = new Order();
-            order.CustomerId = 1;
-            order.Email = "adsf@mail.com";
-            order.Name = "asdf";
-            order.TelephoneNumber = "asdf";
-            order.Price = 12;
-            order.Time = 12;
-            order.Height = 12;
-            order.Length = 12;
-            order.Width = 12;
-            order.CityFrom = "Kapstaden";
-            order.CityTo = "Hvalbugten";
-            order.Date = "01/01/2020";
-
-            /*
             order.CustomerId = customerId;
             order.Email = email;
             order.Name = name;
@@ -42,8 +28,7 @@ namespace Parcel_Delivery_IT_solution.Controllers
             order.CityFrom = cityFrom;
             order.CityTo = cityTo;
             order.Date = date;
-            */
-
+            
             // Insert RouteDTO to database
             DbClient client = new DbClient();
             client.SetOrder(order);
